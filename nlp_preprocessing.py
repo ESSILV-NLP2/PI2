bb18a688bc7a0b883728bced22aa728f1736eb6a
RgzRg <73850576+RgzRg@users.noreply.github.com>
2020-12-13 21:02:30 +0100

    Update nlp_preprocessing.py

--- nlp_preprocessing.py
+++ nlp_preprocessing.py
@@ -42,53 +42,53 @@ class Question_Answering :
       return nlp_fr
 
   def decouperTexte(txt):
-  #Transformation du texte en liste de phrases
-  tokenized_sentence = sent_tokenize(txt)
-  #Taille actuelle de one_input
-  compteur = 0
-  #résultat final
-  all_inputs = []
-  #Une entrée du transformers, i.e une ou plusieurs pharses d'au total 511 caractères ou moins
-  one_input = ""
-  #Pour chaque phrase
-  for sentence in tokenized_sentence :
-    #On récupère la taille de la phrase
-    sentence_size = len(sentence)
-    #Si la phrase à elle seule fait plus de 512 caractères
-    if sentence_size >511:
-      #Pour conserver l'ordre du texte
-      #Si notre entrée actuelle n'est pas vide, on l'ajoute à la liste des entrée
-      if one_input != "":
-        all_inputs.append(one_input)
-        #La nouvelle taille de la nouvelle entrée est donc de 0
-        compteur = 0
-        #Notre nouvelle entrée est vide
-        one_input = ""
-      #Notre phrase trop longue est tronquée
-      sentence = sentence[0:511]
-      #Puis ajoutée à la liste de toutes les entrées
-      all_inputs.append(sentence)
-    #Sinon
-    else :
-      #On regarde si la taille de l'entrée établie jusque là + celle de notre nouvelle phrase est inférieure à 512
-      if compteur + sentence_size < 511:
-        #Si oui la nouvelle dimension de notre entrée est déterminée
-        compteur += sentence_size
-        #On ajpoute la phrase à notre entrée
-        one_input += sentence
+    #Transformation du texte en liste de phrases
+    tokenized_sentence = sent_tokenize(txt)
+    #Taille actuelle de one_input
+    compteur = 0
+    #résultat final
+    all_inputs = []
+    #Une entrée du transformers, i.e une ou plusieurs pharses d'au total 511 caractères ou moins
+    one_input = ""
+    #Pour chaque phrase
+    for sentence in tokenized_sentence :
+      #On récupère la taille de la phrase
+      sentence_size = len(sentence)
+      #Si la phrase à elle seule fait plus de 512 caractères
+      if sentence_size >511:
+        #Pour conserver l'ordre du texte
+        #Si notre entrée actuelle n'est pas vide, on l'ajoute à la liste des entrée
+        if one_input != "":
+          all_inputs.append(one_input)
+          #La nouvelle taille de la nouvelle entrée est donc de 0
+          compteur = 0
+          #Notre nouvelle entrée est vide
+          one_input = ""
+        #Notre phrase trop longue est tronquée
+        sentence = sentence[0:511]
+        #Puis ajoutée à la liste de toutes les entrées
+        all_inputs.append(sentence)
       #Sinon
-      else:
-        #Si on ajoute la phrase à notre entrée, on dépasse les 512 caractères
-        #On ajoute donc notre entrée actuelle à la liste de toutes les entrées
-        all_inputs.append(one_input)
-        #Notre nouvelle entrée actuelle devient la phrase sentence
-        one_input = sentence
-        #La nouvelle taille est la taile de la phrase sentence
-        compteur = sentence_size
-  #Il faut ajouter à la fin la dernière entrée si elle existe
-  if one_input != "":
-    all_inputs.append(one_input)
-  return all_inputs
+      else :
+        #On regarde si la taille de l'entrée établie jusque là + celle de notre nouvelle phrase est inférieure à 512
+        if compteur + sentence_size < 511:
+          #Si oui la nouvelle dimension de notre entrée est déterminée
+          compteur += sentence_size
+          #On ajpoute la phrase à notre entrée
+          one_input += sentence
+        #Sinon
+        else:
+          #Si on ajoute la phrase à notre entrée, on dépasse les 512 caractères
+          #On ajoute donc notre entrée actuelle à la liste de toutes les entrées
+          all_inputs.append(one_input)
+          #Notre nouvelle entrée actuelle devient la phrase sentence
+          one_input = sentence
+          #La nouvelle taille est la taile de la phrase sentence
+          compteur = sentence_size
+    #Il faut ajouter à la fin la dernière entrée si elle existe
+    if one_input != "":
+      all_inputs.append(one_input)
+    return all_inputs
 
 class Language :
   def _calc_ratios(text):
